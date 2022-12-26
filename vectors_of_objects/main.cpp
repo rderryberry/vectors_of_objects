d251c8916627629287db60cfc2eefad65d925cc8
Richard Derryberry <richardtderryberry@gmail.com>
2022-12-26 15:37:36 -0500

    Initial Commit

---
+++ vectors_of_objects/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  vectors_of_objects
+//
+//  Created by Richard Derryberry on 12/26/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
